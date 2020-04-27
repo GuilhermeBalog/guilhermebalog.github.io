d630533ccebc841ab21cef60811a26b173f4b06c
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-04-27 02:36:18 -0300

    Updates

---
+++ precache-manifest.a2292f9582ce1398bfcd4c2c530321c3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58d3b14d28d7af16cd2bbfed0ae2c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c0d6e7d015c4998be32",
+    "url": "/static/css/main.7a65f353.chunk.css"
+  },
+  {
+    "revision": "4144bc49780b37c07cb0",
+    "url": "/static/js/2.7fff3bb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7fff3bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0d6e7d015c4998be32",
+    "url": "/static/js/main.23272315.chunk.js"
+  },
+  {
+    "revision": "c2ffeed580bb82e7b3f3",
+    "url": "/static/js/runtime-main.5e953786.js"
+  },
+  {
+    "revision": "d65fd1dde775d75da8e66cfeeaf132fe",
+    "url": "/static/media/api.d65fd1dd.png"
+  },
+  {
+    "revision": "9bff165e9b682a40fedcc71faebe7c62",
+    "url": "/static/media/capivara-chat.9bff165e.png"
+  },
+  {
+    "revision": "85e9df7c33f1f3d58add517351bb2f6f",
+    "url": "/static/media/dev-radar.85e9df7c.png"
+  },
+  {
+    "revision": "949a2cf4fceb74a59fe51c72e0003f9e",
+    "url": "/static/media/genius.949a2cf4.png"
+  },
+  {
+    "revision": "83e53fecb3db09955b8bf6be0c2974a2",
+    "url": "/static/media/minesweeper.83e53fec.png"
+  },
+  {
+    "revision": "3385d8c968a36a2ac4c227f2ccbcfeca",
+    "url": "/static/media/my-hello-worlds.3385d8c9.png"
+  },
+  {
+    "revision": "f341108f3618f25b667b70482000eb96",
+    "url": "/static/media/portfolio-generator.f341108f.png"
+  },
+  {
+    "revision": "4e2538cc7d719862e6415bab33e86ea9",
+    "url": "/static/media/profile-picture.4e2538cc.png"
+  },
+  {
+    "revision": "8ff74b31d0bc77b1138b77e7be55c1ff",
+    "url": "/static/media/tic-tac-toe.8ff74b31.png"
+  },
+  {
+    "revision": "e0fa569b852a10aec7084899362c3a2a",
+    "url": "/static/media/todolist.e0fa569b.png"
+  }
+]);
