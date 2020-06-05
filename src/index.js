f095fcfb2b6bb28661790ff9aae0ac1d59ea7acc
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-06-05 15:24:26 -0300

    Adiciona react snap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 
 import './index.css'
 
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+
+if(rootElement.hasChildNodes()){
+    hydrate(<App />, rootElement)
+} else {
+    render(<App />, rootElement)
+}
+
