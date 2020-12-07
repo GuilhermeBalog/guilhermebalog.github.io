9d52bfa1a4493264a9095444e6a20312f1efd1aa
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-12-07 15:23:00 -0300

    change css class to camel case

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -7,22 +7,22 @@ import styles from './style.module.css'
 
 function ProjectCard({ project, iconColor }) {
     return (
-        <div className={styles["project-card"]}>
+        <div className={styles.projectCard}>
             <a href={project.homepage}>
-                <img src={project.image} alt={project.name} className={styles["project-image"]} />
+                <img src={project.image} alt={project.name} className={styles.projectImage} />
                 <h4>{project.name}</h4>
             </a>
-            <p className={styles["project-description"]}>
+            <p className={styles.projectDescription}>
                 {project.description}
             </p>
-            <p className={styles["project-language"]}>
+            <p className={styles.projectLanguage}>
                 {project.languages}
             </p>
 
             <SafeLink href={project.repo_url}>
-                <p className={styles["project-repo"]}>
-                    <FaGithub size={16} color={iconColor || '#000'} className={styles["icon"]} />
-                    Repositorie
+                <p className={styles.projectRepo}>
+                    <FaGithub size={16} color={iconColor || '#000'} className={styles.icon} />
+                    Repository
                 </p>
             </SafeLink>
         </div>
