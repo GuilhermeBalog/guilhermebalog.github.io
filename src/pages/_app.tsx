a2e29026fdaf5e4889ab78cccaa30c99d19efc87
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-01-20 14:21:45 -0300

    Remove Roboto font and update site url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,13 +13,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#5e22af" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,800;1,300&display=swap"
+          rel="stylesheet"
+        />
 
         <title>Guilherme Balog Gardino | Software Developer</title>
         <meta
           name="description"
           content="I am a Software Developer, in love with tech solutions"
         />
-
         <meta
           itemProp="name"
           content="Guilherme Balog Gardino | Software Developer"
@@ -32,7 +36,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           itemProp="image"
           content="http://avatars0.githubusercontent.com/u/38947601?v=4"
         />
-
         <meta
           property="og:title"
           content="Guilherme Balog Gardino | Software Developer"
@@ -42,7 +45,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="Guilherme is a Software Developer, in love with tech solutions"
         />
         <meta property="og:site_name" content="Guilherme Balog Gardino" />
-        <meta property="og:url" content="https://guilhermebalog.github.io" />
+        <meta property="og:url" content="https://guilhermebalog.ga" />
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
@@ -51,7 +54,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="454" />
         <meta property="og:image:height" content="454" />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
