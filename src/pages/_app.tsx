d960063b33022fd73a24bd3ac56f7a681e3bb1a2
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-01-31 15:55:37 -0300

    Refactor head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import Head from 'next/head';
 
 import '../styles/index.css';
 
+const title = 'Guilherme Balog Gardino | Desenvolvedor de software';
+const description =
+  'Olá! Meu nome é Guilherme Balog Gardino e sou desenvolvedor de software em São Paulo, Brasil. Trabalho principalmente como programador fullstack usando Node.js e React.js, mas gosto muito de experimentar diferentes ferramentas e linguagens e com elas construir projetos inusitados.';
+const image = 'https://avatars0.githubusercontent.com/u/38947601?v=4';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -10,7 +15,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#5e22af" />
+        <meta name="theme-color" content="#00BCD4" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -19,54 +24,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
 
-        <title>Guilherme Balog Gardino | Desenvolvedor de software</title>
-        <meta
-          name="description"
-          content="Olá! Meu nome é Guilherme Balog Gardino e sou desenvolvedor de software em São Paulo, Brasil. Trabalho principalmente como programador fullstack usando Node.js e React.js, mas gosto muito de experimentar diferentes ferramentas e linguagens e com elas construir projetos inusitados."
-        />
-        <meta
-          itemProp="name"
-          content="Guilherme Balog Gardino | Desenvolvedor de software"
-        />
-        <meta
-          itemProp="description"
-          content="Olá! Meu nome é Guilherme Balog Gardino e sou desenvolvedor de software em São Paulo, Brasil. Trabalho principalmente como programador fullstack usando Node.js e React.js, mas gosto muito de experimentar diferentes ferramentas e linguagens e com elas construir projetos inusitados."
-        />
-        <meta
-          itemProp="image"
-          content="http://avatars0.githubusercontent.com/u/38947601?v=4"
-        />
-        <meta
-          property="og:title"
-          content="Guilherme Balog Gardino | Desenvolvedor de software"
-        />
-        <meta
-          property="og:description"
-          content="Guilherme is a Software Developer, in love with tech solutions"
-        />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={image} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
         <meta property="og:site_name" content="Guilherme Balog Gardino" />
         <meta property="og:url" content="https://guilhermebalog.ga" />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://avatars0.githubusercontent.com/u/38947601?v=4"
-        />
+        <meta property="og:image" content={image} />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="454" />
-        <meta property="og:image:height" content="454" />
+        <meta property="og:image:width" content="460" />
+        <meta property="og:image:height" content="460" />
+
         <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Guilherme Balog Gardino | Desenvolvedor de software"
-        />
-        <meta
-          name="twitter:description"
-          content="Olá! Meu nome é Guilherme Balog Gardino e sou desenvolvedor de software em São Paulo, Brasil. Trabalho principalmente como programador fullstack usando Node.js e React.js, mas gosto muito de experimentar diferentes ferramentas e linguagens e com elas construir projetos inusitados."
-        />
-        <meta
-          name="twitter:image"
-          content="http://avatars0.githubusercontent.com/u/38947601?v=4"
-        />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <Component {...pageProps} />
     </>
