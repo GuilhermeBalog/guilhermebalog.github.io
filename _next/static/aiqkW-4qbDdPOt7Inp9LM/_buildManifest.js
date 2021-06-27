cdf816bfc262e5059ef454ca22b97fab10a2521c
GuilhermeBalog <GuilhermeBalog@users.noreply.github.com>
2021-06-27 00:42:48 +0000

    deploy: 57ebc025168970099d6f1a6c20bd413d6b81c774

---
+++ _next/static/aiqkW-4qbDdPOt7Inp9LM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-29661574a23ec7d7a17a.js","static/chunks/pages/index-bfcad44543f2db741be3.js"],"/_error":["static/chunks/pages/_error-1f5220881bd5a88b0329.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
